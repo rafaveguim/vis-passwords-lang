ce61336e941f3f46b8e00907b1d359edbf089733
Rafael Veras <rafaveguim@gmail.com>
2012-05-16 12:15:58 -0400

    Added Wordle and did a lot of refactoring.

--- pwdvis_client/WebContent/calendar/calendar.js
+++ pwdvis_client/WebContent/calendar/calendar.js
@@ -17,7 +17,7 @@ var day     = d3.time.format("%w"),
     mmddFormat  = d3.time.format("%m-%d");
 
 
-var rows, freqByDay;
+var rows, days, daysOfYear;
 
 var colorDay;
 
@@ -30,13 +30,71 @@ function start(){
         cellSize = (width('chart') - margin.right - margin.left)/53;
 
         this.rows = rows;
-        freqByDay = nestByDate(rows);
+        days  = frewByDate(rows);
+        daysOfYear = freqByDayOfYear(rows);
+
         colorDay = d3.scale.quantile()
-                    .domain(d3.values(freqByDay))
+                    .domain(d3.values(days))
                     .range(d3.range(9));
+
         drawOverallCalendar();
         drawBall();
+        //drawWordle();
     })
+}
+
+function drawWordle(date){
+    var set = rows.filter(function(d){return date.getFullYear()==d.YEAR
+        && date.getMonth()== d.MONTH-1
+        && date.getDate()== d.DAY;});
+
+    var fontSize = d3.scale.log()
+           .domain(d3.extent(set.map(function(d){return +d.PWD_FREQUENCY;})))
+           .range([20,80]);
+
+    var words = set.map(function(d){
+        return {text: d.RAW, size: fontSize(+d.PWD_FREQUENCY)};
+    });
+
+    var w = width('wordle'), h = height('wordle');
+    d3.layout.cloud().size([w, h])
+        .words(words)
+        .font('Trebuchet MS')
+        .rotate(function() { return /*~~(Math.random() * 2) * 90*/0; })
+        .fontSize(function(d) { return d.size; })
+        .on("end", draw)
+        .start();
+}
+
+function draw(words){
+    var w = width('wordle'), h = height('wordle');
+
+    d3.select('#wordle').selectAll('svg')
+        .data([null])
+        .enter().append('svg')
+        .attr('width', w)
+        .attr('height', h)
+        .append('g')
+        .attr('transform', 'translate('+w/2+','+h/2+')');
+
+    svg = d3.select('#wordle').selectAll('svg').select('g');
+
+    var setter = function(sel){
+        sel.style("font-size", function(d) { return d.size + "px"; })
+            .style('font-family', 'Trebuchet MS')
+            .attr("text-anchor", "middle")
+            //.attr('text-rendering', 'optimizeSpeed')
+            .attr("transform", function(d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function(d) { return d.text; });
+    }
+
+    var bound = svg.selectAll('text').data(words);
+    bound.exit().remove();
+    bound.enter().append("text");
+
+    svg.selectAll('text').call(setter);
 }
 
 /**
@@ -46,22 +104,33 @@ function start(){
  * Each orbit corresponds to a decade.
  */
 function drawBall(){
-    var w = 1000,
-        h = 500;
-    var freqByYear = freqByYear_(rows);
+    var w = width('ball'), h = height('ball');
+
+    var freqByYear = freqByYear_(rows),
+        decades_   = decades(d3.keys(freqByYear).sort());
 
     var color = d3.scale.quantile()
         .domain(d3.values(freqByYear))
         .range(d3.range(9));
 
     var svg = d3.select("#ball").append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .attr("class", "YlGnBu")
-        .append("g")
-        .attr("transform","translate(300,250)"); // refer to ColorBrewer CSS
+                .attr("width", w)
+                .attr("height", h)
+                .attr("class", "YlGnBu"); // refer to ColorBrewer CSS
 
-    var decades_ = decades(d3.keys(freqByYear).sort());
+    // a base layer to capture 'outside' clicks
+    svg.append('rect')
+        .attr('class', 'invisible')
+        .attr('width' , w)
+        .attr('height', h)
+        .on('click', function(){
+            selection.attr('display','none');
+            drawOverallCalendar();
+        });
+
+    // everything is drawn in this shifted g
+    svg = svg.append("g")
+             .attr('transform','translate('+w/2+','+h/2+')');
 
     // computes radius for an orbit o
     var radius = function(o){return o*17+17;};
@@ -74,21 +143,21 @@ function drawBall(){
         .selectAll("circle")
         .data(d3.range(decades_.length))
         .enter().append("circle")
-        .attr("cy",0)
-        .attr("cx",0)
         .attr("r", radius);
-    
+
+    // appending 'internal labels' (1940s, 1990s..)
     svg.append('g')
     	.attr('class', 'internal_label')
     	.selectAll('text')
     	.data(decades_.map(function(a){return a[0];}))
     	.enter().append('text')
-    	.text(function(d,i){return i<3 ? '' : d+'\'s';})
+    	.text(function(d,i){return i<3 ? '' : d+'s';})
     	.attr('transform', function(d,i){return	'rotate(0)'
     		+ 'translate(0,3)'
     		+ 'translate(0,'+ radius(i) +') ';})
     	.attr('text-anchor', 'middle');
-    
+
+    // 'external labels' (1, 2, 3... 10)
     svg.append("g")
         .selectAll('text')
 	    .data(d3.range(10))
@@ -103,56 +172,69 @@ function drawBall(){
 	     })
 	    .text(String);
 
+    var hover = svg.append('circle')
+        .attr('class','hover')
+        .attr('display','none')
+        .attr('r',7);
+
     // two-levels of data-binding here. decades -> svg:g; years -> svg:circle
     svg.selectAll("g.decade")
         .data(decades_)
-        .enter().append("g")
+        .enter()
+        .append("g")
         .attr("class", "decade")
-      .selectAll("circle.year")
+      .selectAll("circle")
         .data(function(a,i){ // cross decades with years
             return a.map(function(d,j){
                 return {year:+d, decade:i, freq: freqByYear[+d]}
             })
         })
-        .enter().append("circle")//.attr('class', 'year')
+        .enter()
+        .append("circle")
         .attr("cx", function(d,i){ return radius(d.decade)*Math.cos(angle(i)); })
         .attr("cy", function(d,i){ return radius(d.decade)*Math.sin(angle(i)); })
         .attr("r", 4)
         .attr("class", function(d) { return "q" + color(d.freq) + "-9"; })
-        .on("mouseover", function(d){ drawCalendar([d.year]); })
+        .on("click", function(d){
+            drawCalendar(d.year);
+            align(selection, this);
+            selection.style('fill', d3.select(this).style('fill'));
+        })
+        .on("mouseover", function(){ align(hover, this); })
+        .on("mouseout", function(){ hover.attr('display','none') })
         .append("title")
-        .text(function(d){return ""+d.year;});
-       
+        .text(function(d){return d.year});
+
+    var selection = svg.append('circle')
+            .attr('class','selection')
+            .attr('display','none')
+            .attr('r',7);
 }
 
 function drawOverallCalendar(){
-    var w  = width('chart'), h = height('chart');
-
-    var daysOfYear = freqByDayOfYear(rows),
+    var w  = width('chart'), h = height('chart'),
+        year = 1967,
         color = d3.scale.quantile()
                 .domain(d3.values(daysOfYear))
                 .range(d3.range(9));
 
-    var svg = d3.select("#chart").selectAll("svg");
-    svg.data([]).exit().remove();
+    d3.select('#chart').select('svg').remove(); // cleaning
 
-    svg = d3.select("#chart").selectAll("svg")
-        .data([1967]) // a common year starting on Sunday
-        .enter().append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .attr("class", "YlGnBu")
-        .append("g")
-        .attr("transform", "translate(" + margin.left + ","
-        + (margin.top + (h - cellSize * 7) / 2) + ")");
+    var svg = d3.select("#chart").append("svg")
+                .attr("width", w)
+                .attr("height", h)
+                .attr("class", "YlGnBu")
+                .append("g")
+                .attr("transform", "translate(" + margin.left + ","
+                + (margin.top + (h - cellSize * 7) / 2) + ")");
 
     svg.append("text")
         .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
         .attr("text-anchor", "middle")
-        .text("Overall");
+        .text("Overview");
 
     var rect = svg.selectAll("rect.day")
-        .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .data(function(d) { return d3.time.days(new Date(year, 0, 1), new Date(year + 1, 0, 1)); })
         .enter().append("rect")
         .attr("class", "day")
         .attr("width", cellSize)
@@ -165,55 +247,64 @@ function drawOverallCalendar(){
         .text(function(d) { return d + ": " + daysOfYear[d]; });
 
     svg.selectAll("path.month")
-        .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .data(function() { return d3.time.months(new Date(year, 0, 1), new Date(year + 1, 0, 1)); })
         .enter().append("path")
         .attr("class", "month")
         .attr("d", monthPath);
 }
 
-function drawCalendar(years){
+function drawCalendar(year){
     var w  = width('chart'), h = height('chart');
 
-    var svg = d3.select("#chart").selectAll("svg");
-    svg.data([]).exit().remove();
+    d3.select('#chart').select('svg').remove(); // cleaning
 
-    svg = d3.select("#chart").selectAll("svg")
-        .data(years)
-        .enter().append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .attr("class", "YlGnBu")
-        .append("g")
-        .attr("transform", "translate(" + margin.left + ","
-        + (margin.top + (h - cellSize * 7) / 2) + ")");
+    var svg = d3.select("#chart").append("svg")
+            .attr("width", w)
+            .attr("height", h)
+            .attr("class", "YlGnBu")
+            .append("g")
+            .attr("transform", "translate(" + margin.left + ","
+            + (margin.top + (h - cellSize * 7) / 2) + ")");
 
     svg.append("text")
         .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
         .attr("text-anchor", "middle")
-        .text(String);
+        .text(year);
 
     var rect = svg.selectAll("rect.day")
-        .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .data(function() { return d3.time.days(new Date(year, 0, 1), new Date(year + 1, 0, 1)); })
         .enter().append("rect")
         .attr("class", "day")
         .attr("width", cellSize)
         .attr("height", cellSize)
         .attr("x", function(d) { return week(d) * cellSize; })
-        .attr("y", function(d) { return day(d) * cellSize; });
+        .attr("y", function(d) { return day(d) * cellSize; })
+        .on('click', function(d){
+            drawWordle(d);
+        });
 
     rect.append("title")
         .text(function(d) { return fullFormat(d) + ': 0'; });
 
     svg.selectAll("path.month")
-        .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .data(function() { return d3.time.months(new Date(year, 0, 1), new Date(year + 1, 0, 1)); })
         .enter().append("path")
         .attr("class", "month")
         .attr("d", monthPath);
 
-    rect.filter(function(d) { return d in freqByDay; })
-        .attr("class", function(d) { return "day q" + colorDay(freqByDay[d]) + "-9"; })
+    rect.filter(function(d) { return d in days; })
+        .attr("class", function(d) { return "day q" + colorDay(days[d]) + "-9"; })
         .select('title')
-        .text(function(d) { return fullFormat(d) + ": " + freqByDay[d]; });
+        .text(function(d) { return fullFormat(d) + ": " + days[d]; });
+}
+
+/**
+ * Aligns two circles
+ */
+function align(c1, c2){
+    c1.attr('display', null);
+    c1.attr('cx', d3.select(c2).attr('cx'));
+    c1.attr('cy', d3.select(c2).attr('cy'));
 }
 
 function width(el) { return document.getElementById(el).clientWidth;  }
@@ -275,7 +366,7 @@ function freqByYear_(rows){
         .map(rows);
 }
 
-function nestByDate(rows){
+function frewByDate(rows){
     return d3.nest()
         .key(function(d){return new Date(d.YEAR,d.MONTH-1,d.DAY);})
         .rollup(function(d){return +d[0].DATE_FREQUENCY;})
